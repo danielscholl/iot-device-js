c9f9307469d60302ca8296bbc84257404de13401
Daniel Scholl <daniel@scholllopez.com>
2019-02-16 13:16:38 -0600

    Fixes for Interval Setting in DirectMethod

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Device = require('./lib/device');
 const Telemetry = require('./lib/models').Device;
 const config = {
   connectionString: process.env.DEVICE_CONNECTION_STRING,
-  interval: process.env.MESSAGE_INTERVAL || 1000,
+  interval: process.env.MESSAGE_INTERVAL * 1000 || 2000,
   options: {}
 };
 
